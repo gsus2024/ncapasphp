8d5c11d3c0ac2473d087c6d03def7d7d9a497cf4
gsus2024 <gsusromerovillanueva2024@gmail.com>
2025-06-05 07:51:29 -0500

    se agrega funcionalidad categoria

--- presentacion/cargarfamilias.php
+++ presentacion/cargarfamilias.php
@@ -1,7 +1,39 @@
-<?php
-    require '../logica/LFamilia.php';
-    $log=new LFamilia();
-    foreach($log->cargar() as $familia){
-        echo $familia->getNombre();
-    }
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <h1>Listado de Familias</h1>
+        <hr>
+        <a href="guardarfamilia.php">Crear Nuevo</a>
+        <table>
+            <thead>
+                <tr>
+                    <td>Id</td>
+                    <td>Nombre</td>
+                    <td>Descripción</td>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    require '../logica/LFamilia.php';
+                    $log=new LFamilia();
+                    foreach($log->cargar() as $familia){
+                ?>
+                <tr>
+                    <td><?=$familia->getIdFamilia()?></td>
+                    <td><?=$familia->getNombre()?></td>
+                    <td><?=$familia->getDescripcion()?></td>
+                </tr>
+                <?php
+                    }
+                ?>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
--- presentacion/guardarfamilia.php
+++ presentacion/guardarfamilia.php
@@ -1,9 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <h1>Inserción de Familias</h1>
+        <hr>
+        <form action="" method="post">
+            <input type="text" name="txtNom" placeHolder="Nombre">
+            <input type="text" name="txtDes" placeHolder="Descripción">
+            <input type="submit" value="guardar">
+        </form>
+    </div>
+</body>
+</html>
 <?php
     require '../logica/LFamilia.php';
-    $fam=new Familia();
-    $fam->setNombre("aaa");
-    $fam->setDescripcion("bbb");
-    $log=new LFamilia();
-    $log->guardar($fam);
-    echo 'DATOS GUARDADOS';
+    if($_POST){
+        $fam=new Familia();
+        $fam->setNombre($_POST['txtNom']);
+        $fam->setDescripcion($_POST['txtDes']);
+        $log=new LFamilia();
+        $log->guardar($fam);
+        header('Location: cargarfamilias.php');
+    }
 ?>
